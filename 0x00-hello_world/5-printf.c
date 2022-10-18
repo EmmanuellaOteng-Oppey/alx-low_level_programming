ae1b2606cbf91492b47993f5e6ad4495b43d1047
EmmanuellaOteng-Oppey <emmanuellaoteng@gmail.com>
2022-10-18 01:28:38 -0700

    5print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-
-
 /**
- *
- * * main - Prints a text
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of
-
-			art,\n");
-
+	printf("with proper grammer, but the outcome is a piece of art, \n");
 	return (0);
-
 }
+
