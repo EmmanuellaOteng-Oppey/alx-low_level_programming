e82f8ca4528306da98fa17ca772550ba28559a91
EmmanuellaOteng-Oppey <emmanuellaoteng@gmail.com>
2022-10-24 10:29:01 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
